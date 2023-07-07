f89f7071c4b705d99afac0b2e90d4e3a76ca4898
emifa <emifa@LAPTOP-1TM449QB>
2023-07-07 15:27:13 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-5-LINQ--.NET/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        List<double> numbers =  new List<double> {2.5,7.5,6.2,82.6,42,46.2,12.5 };
+
+        double sum = numbers.Sum();
+        double avg = numbers.Average();
+        int count = numbers.Count();
+
+        Console.WriteLine("Sum : " + sum);
+        Console.WriteLine("Avg : " + avg);
+        Console.WriteLine("Count items : " + count);
+
+    }
+}
